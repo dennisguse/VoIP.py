11b11ccbc3d5e31a544c06ddbef3df38d479bfb3
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2013-12-17 11:33:31 +0100

    Refactoring AudioDeviceChooser

--- Modules/UIModules/DeviceChooserModule.py
+++ Modules/UIModules/DeviceChooserModule.py
@@ -4,6 +4,7 @@ from PyQt4 import uic
 from Modules.ConfigModules.AudioDeviceModule import AudioDeviceModule
 import Modules.UIModules.RESOURCES as UIResources
 import pjsua
+import logging
 
 class DeviceChooserModule(QDialog):
 
@@ -26,20 +27,20 @@ class DeviceChooserModule(QDialog):
 
     def initDialog(self):
         self.dia = uic.loadUi(UIResources.RESCOURCES_UI["DeviceChooserDialog"], self)
-        self.dia.setWindowTitle('Device Chooser')
-        self.dia.labelDev1.setText("Capture:")
-        self.dia.labelDev2.setText("Record:")
         self.dia.btn.clicked.connect(self.save)
         self.lib = pjsua.Lib.instance()
         sndDevices = self.lib.enum_snd_dev()
         for device in sndDevices:
-            if "(hw:" in device.name:
-                self.dia.cmbDev1.addItem(device.name)
-                self.dia.cmbDev2.addItem(device.name)
+            if "(hw:" not in device.name: #Valid deviceId available?
+                 continue
+            if device.input_channels > 0:
+                 self.dia.cmbDeviceInput.addItem(device.name)
+            if device.output_channels > 0:
+                 self.dia.cmbDeviceOutput.addItem(device.name)
 
     def save(self):
-        self.lib.set_snd_dev(self.getId(self.dia.cmbDev1.currentText(), 0),\
-                                           self.getId(self.dia.cmbDev2.currentText(), 1))
+        self.lib.set_snd_dev(self.getId(self.dia.cmbDeviceInput.currentText(), 0),\
+                                           self.getId(self.dia.cmbDeviceOutput.currentText(), 1))
         self.dia.close()
         self.showWaring()
 
@@ -50,6 +51,7 @@ class DeviceChooserModule(QDialog):
             return int(text.split("(hw:")[1][2])
 
     def showWaring(self):
+	return
         diaWaring = QMessageBox()
         diaWaring.setWindowTitle('Attention!')
         diaWaring.setText('Because of the incredible laziness of the developer you have to set the device id by your self in the file:\n Modules.ConfigModules.AudioDeviceModule\nIf you want to avoid if dialog')
