0ec55a12001ba70462d90bc4cce86316ba09e53b
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2014-01-28 18:33:33 +0100

    Updated TODO (mainly moved to Github-repository). Fixed setup.py (added license, +x)

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python2
+
 from distutils.core import setup
 setup(
     name="VoIP.py",
     version="0.1",
-    description="A highly re-configurable VoIP-Client framework using PJSIP).",
+    description="A highly re-configurable VoIP-Client framework using PJSIP.",
     author=["Frank Haase", "Dennis Guse"],
     packages = ['', 'Defines', 'SIPController', 'Modules', 'Modules/CallModules', 'Modules/ConfigModules', 'Modules/MainCliModules', 'Modules/MainUIModules', 'Modules/UIModules', 'Modules/PresenceModules'],
     package_data={'' : ['UIElements/*', 'Resources/*']},
+    license='GNU GPL',
 )
